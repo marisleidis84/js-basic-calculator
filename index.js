bbca0d31b2cbb44dd4cab05fe4614013129507d0
Orlando <monroyorlando.36@gmail.com>
2020-12-30 15:48:15 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+/*
+  Crea una calculadora basica usando funciones que permita, sumar, restar, multiplicar y dividir.
+
+  Cada operacion debe estar en una funcion por separado.
+
+  La funcion llamada "calculate" debe mostrar en consola en valor de la operacion. 
+*/
+
+
+//Numeros a operar
+let sumar = {
+    a: 30,
+    b: 60
+}
+
+let restar = {
+    a: 10000,
+    b: 578
+}
+
+let multiplicar = {
+    a: 3.54,
+    b: 30
+}
+
+let dividir = {
+    a: 100,
+    b: 6
+}
+
+//Tu funcion de suma aqui abajo
+
+
+//Tu funcion de resta aqui abajo
+
+
+//Tu funcion de  multiplicar aqui abajo
+
+
+//Tu funcion de dividir aqui abajo
+
+
+
+//Funcion calculate
+window.onload = function(){
+    console.log("Me ejecute al cargar la pagina")
+    //Ejecuta tu funcion "calculate" dentro de este bloque de codigo
+}
+
+function calculate(func, obj){
+    //Tu logica va aqui 
+}
